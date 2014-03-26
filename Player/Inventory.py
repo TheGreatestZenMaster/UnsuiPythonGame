8ef05fe7a9aab132fd3aa5162c7a726eece024a5
Jake Loose <jetloose@gmail.com>
2014-03-26 09:40:14 +0900

    Update Inventory.py
    Added print dialog, a remove item function and a current inventory function.

--- Player/Inventory.py
+++ Player/Inventory.py
@@ -1,7 +1,25 @@
-
 class Inventory(object):
     def __init__(self):
         self.items = list()
-    def add_item(self,item):
+
+    def add_item(self, item):
         if item != None:
             self.items.append(item)
+            print "The %s was added to your inventory!" % item.name
+
+    def remove_item(self, item):
+        for remove_item in self.items:
+            if item == remove_item.name:
+                self.items.remove(remove_item)
+                print "The %s was removed from your inventory." % remove_item.name
+                break
+            else:
+                print "something is wrong"
+
+    def current_inventory(self):
+        if len(self.items) > 0:
+            for item in self.items:
+                print "In your inventory, there is a...", item.name
+        else:
+            print "Your inventory is empty!"
+        
