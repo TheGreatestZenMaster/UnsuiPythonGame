ab06d81358e56d4278078d1fbf05b04a5b33032a
Jake Loose <jetloose@gmail.com>
2014-03-26 09:56:49 +0900

    Update Room.py
    Added a couple of functions that perform print dialogs about the room.

--- rooms/Room.py
+++ rooms/Room.py
@@ -12,3 +12,14 @@ class Room(object):
         self.number = number
         self.name = name
         self.victory = victory
+        self.objects = list()
+
+    def on_entering_room(self):
+        print "You have entered the %s" % self.name
+
+    def objects_in_room(self):
+        if len(self.objects) > 0:
+            for room_object in self.objects:
+                print "There is %s" % room_object.name
+        else:
+            print "The room seems to be empty"
