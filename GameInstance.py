2d8da23c2538ece3a1468d9010da8aef4ce2d5f0
Tylar Murray <murray.tylar@gmail.com>
2014-03-26 20:39:20 -0400

    changed to use .conf files rather than hard-coded rooms

--- GameInstance.py
+++ GameInstance.py
@@ -19,24 +19,6 @@ class GameInstance(object):
     def __init__(self):
         self.actions_available = BASE_ACTIONS
 
-        self.keys = [Key("Key", "Room 1", 10001),
-                        Key("Key", "Room 2", 20002),
-                        Key("Key", "Room 3", 30003)]
-
-        self.rooms = [Room("Room 1", 1, False),
-                         Room("Room 2", 2, False),
-                         Room("Room 3", 3, False),
-                         Room("Room 4", 4, False)]
-
-        self.doors = [Door("Door 1", 1, False, None),
-                         Door("Door 2", 2, True, 10001),
-                         Door("Door 3", 3, True, 20002),
-                         Door("Door 4", 4, True, 30003)]
-
-        self.monsters = [Wolf("wolf", "Room 2"),
-                            Wolf("wolf", "Room 3"),
-                            Wolf("wolf", "Room 4")]
-
         self.player = Player("NoName", "Male", "Human", None)
 
         self.config_loader = UnsuiConfigLoader()
@@ -100,7 +82,6 @@ class GameInstance(object):
             return True
         else:
             print "That's not a valid command!!!"
-        
         
 # NOTE: code below here is out-of-date and needs to be updated for use here. ~7yl4r
 
