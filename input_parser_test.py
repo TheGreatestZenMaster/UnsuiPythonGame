363ceabd840c289f22b3c61b578e4c7e9963282a
Joshua Rowe <joshuarowe1@icloud.com>
2014-03-30 14:33:43 +1100

    Added a few more tokenise tests

--- input_parser_test.py
+++ input_parser_test.py
@@ -1,6 +1,9 @@
 import unittest
 
+from nose.tools import *
 from input_parser import Command, Verb, Object, Parser
+
+parser = Parser()
 
 class basic_class_tests(unittest.TestCase):
 
@@ -28,3 +31,16 @@ class basic_class_tests(unittest.TestCase):
             print res, '=?=', TEST_RESULTS[i]
             self.assertTrue(res == TEST_RESULTS[i])
         
+def parser_tokenise_test():
+
+    # Basic tokenising 
+    assert_equal(parser.tokenise('use key on door'), [('verb', 'use'), ('noun', 'key'), ('preposition', 'on'), ('noun', 'door')])
+    assert_equal(parser.tokenise('pet cat on bed'), [('verb', 'pet'), ('noun', 'cat'), ('preposition', 'on'), ('noun', 'bed')])
+
+    # Article stripping
+    assert_equal(parser.tokenise('go to the hallway'), [('verb', 'go'), ('preposition', 'to'), ('stop', 'the'), ('noun', 'hallway')])
+    assert_equal(parser.tokenise('eat a banana'), [('verb', 'eat'), ('noun', 'banana')])
+
+    # Test Caps and Errors
+    assert_equal(parser.tokenise('MAP'), [('command', 'map')])
+    assert_equal(parser.tokenise('tHis IS CrAZy'), [('error', 'this'), ('error', 'is'), ('error', 'crazy')])
